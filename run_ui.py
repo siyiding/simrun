f664076c8caf7bb4e7bd556950c23d597ed5249d
yidingsi <bestsi222@gmail@.com>
2026-03-16 21:32:05 +0800

    fix: 设置matplotlib中文字体

--- run_ui.py
+++ run_ui.py
@@ -1,6 +1,12 @@
 import sys
+
+import matplotlib
 from PySide6.QtWidgets import QApplication
 from ui.main_window import MainWindow
+
+# 全局设置 Matplotlib 中文字体，避免中文乱码/缺字
+matplotlib.rcParams["font.sans-serif"] = ["Microsoft YaHei", "SimHei"]
+matplotlib.rcParams["axes.unicode_minus"] = False
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
